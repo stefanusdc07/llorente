451927f249658bdf07b9d70008800eb5b2ba53b4
stefanusdc07 <56071960+stefanusdc07@users.noreply.github.com>
2023-10-05 10:18:56 +0700

    Add Balance Status for Sales report

--- domain/Shop/Order/Enums/PaymentStatus.php
+++ domain/Shop/Order/Enums/PaymentStatus.php
@@ -4,26 +4,13 @@ declare(strict_types=1);
 
 namespace Domain\Shop\Order\Enums;
 
-use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 use Illuminate\Support\Str;
 
-enum PaymentStatus: string implements HasColor, HasLabel
+enum PaymentMethod: string implements HasLabel
 {
-    case CANCELED = 'canceled';
-    case FAILED = 'failed';
-    case PAID = 'paid';
-    case PENDING = 'pending';
-    case UNPAID = 'unpaid';
-
-    public function getColor(): string|array|null
-    {
-        return match ($this) {
-            self::PAID => 'success',
-            self::PENDING => 'warning',
-            self::CANCELED, self::FAILED, self::UNPAID => 'danger',
-        };
-    }
+    case CASH = 'cash';
+    case GCASH = 'Authorize.net';
 
     public function getLabel(): ?string
     {
