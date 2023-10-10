e98029c46f61db35fa9081f4f9268908be5bd795
stefanusdc07 <56071960+stefanusdc07@users.noreply.github.com>
2023-10-10 13:57:44 +0700

    Fixing Balance Status

--- domain/Shop/Order/Enums/PaymentStatus.php
+++ domain/Shop/Order/Enums/PaymentStatus.php
@@ -4,13 +4,26 @@ declare(strict_types=1);
 
 namespace Domain\Shop\Order\Enums;
 
+use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 use Illuminate\Support\Str;
 
-enum PaymentMethod: string implements HasLabel
+enum PaymentStatus: string implements HasColor, HasLabel
 {
-    case CASH = 'cash';
-    case GCASH = 'Authorize.net';
+    case CANCELED = 'canceled';
+    case FAILED = 'failed';
+    case PAID = 'paid';
+    case PENDING = 'pending';
+    case UNPAID = 'unpaid';
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::PAID => 'success',
+            self::PENDING => 'warning',
+            self::CANCELED, self::FAILED, self::UNPAID => 'danger',
+        };
+    }
 
     public function getLabel(): ?string
     {
