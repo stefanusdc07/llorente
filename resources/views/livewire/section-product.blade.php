12a98e53d45bba82ddb0dd6e9f7d6043a9f96cd9
andy-susanto <officialandysusanto@gmail.com>
2023-10-19 17:04:01 +0700

    update image

--- resources/views/livewire/section-product.blade.php
+++ resources/views/livewire/section-product.blade.php
@@ -2,7 +2,7 @@
     @foreach ($product as $dataProduct)
         <section class="mb-10 mt-10" wire:key="{{ $dataProduct->id }}">
             <div class="border border-white p-4 rounded-lg">
-                <img src="{{ $dataProduct->getFirstMediaUrl() }}"
+                <img src="{{ $dataProduct->getFirstMediaUrl('image') }}"
                     class="mb-6 w-full rounded-lg shadow-lg dark:shadow-black/20" alt="image" />
                 <div class="text-center text-white">
                     <h1 class="mb-6 text-3xl font-bold text-white">
