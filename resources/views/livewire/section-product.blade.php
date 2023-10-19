5f2e629295d42ca2eea0ef7024e6e84bbb6b66b9
andy-susanto <officialandysusanto@gmail.com>
2023-10-19 17:15:17 +0700

    update image

--- resources/views/livewire/section-product.blade.php
+++ resources/views/livewire/section-product.blade.php
@@ -1,8 +1,8 @@
 <div>
     <div class="flex flex-row">
         @foreach ($product as $dataProduct)
-            <section class=" mb-10 mt-10" wire:key="{{ $dataProduct->id }}">
-                <div class="basis-1/4 border border-white p-4 rounded-lg">
+            <section class="basis-1/4 ml-2 mr-2 mb-10 mt-10" wire:key="{{ $dataProduct->id }}">
+                <div class="border border-white p-4 rounded-lg">
                     <img src="{{ $dataProduct->getFirstMediaUrl('image') }}"
                         class="mb-6 rounded-lg shadow-lg dark:shadow-black/20" alt="image" />
                     <div class="text-center text-white">
